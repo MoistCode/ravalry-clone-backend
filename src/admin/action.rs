4dd3552267bd0eb386c3de362aea388368a98855
Tommy Long Pham <tpham@smugmug.com>
2022-01-20 14:45:36 -0800

    Add admin routes and actions

---
+++ src/admin/action.rs
@@ -0,0 +1,142 @@
+use actix_web::{web};
+use bcrypt::{DEFAULT_COST, hash};
+use diesel::prelude::*;
+use fake::{Dummy, Fake, Faker};
+use rand::Rng;
+use std::collections::HashSet;
+use uuid::Uuid;
+
+type DbError = Box<dyn std::error::Error + Send + Sync>;
+
+
+/// Populates the database with users.
+fn populate_users(conn: &SqliteConnection) -> Result<Option<Vec<String>>, DbError> {
+    use fake::faker::name::raw::*;
+    use fake::faker::internet::raw::*;
+    use fake::locales::*;
+
+    use crate::user;
+    use crate::schema::users::dsl::*;
+
+    let num_of_users = 50;
+    let mut user_ids = vec![];
+
+    for n in 1..=num_of_users {
+        println!("Inserting user: {}/{}", n, num_of_users);
+        let user_id = Uuid::new_v4().to_string();
+        let pw: String = Password(EN, 12..32).fake();
+        let hash_pw = hash(pw, DEFAULT_COST)?;
+
+        let new_user = user::models::User {
+            id: user_id.to_owned(),
+            first_name: FirstName(EN).fake(),
+            last_name: LastName(EN).fake(),
+            username: Username(EN).fake(),
+            password: hash_pw,
+        };
+        
+        diesel::insert_into(users).values(&new_user).execute(conn)?;
+        user_ids.push(user_id);
+        println!("Completed user: {}/{}", n, num_of_users);
+    }
+
+    Ok(Some(user_ids))
+}
+
+/// Populates the database with patterns. A random number of patterns are
+/// generated per user.
+fn populate_patterns(
+    conn: &SqliteConnection,
+    user_ids: &Vec<String>
+) -> Result<Option<Vec<String>>, DbError> {
+    use fake::faker::lorem::raw::*;
+    use fake::faker::chrono::raw::*;
+    use fake::locales::*;
+    use rand::prelude::*;
+
+    use crate::pattern;
+    use crate::schema::patterns::dsl::*;
+
+    let mut rng = rand::thread_rng();
+    let num_of_patterns = 150;
+    let mut pattern_ids = vec![];
+    let user_ids_len = user_ids.len();
+
+    for n in 1..=num_of_patterns {
+        println!("Inserting pattern: {}/{}", n, num_of_patterns);
+        let random_index = rng.gen_range(0..user_ids_len);
+        let random_user_id = &user_ids[random_index];
+        let pattern_id = Uuid::new_v4().to_string();
+
+        let new_pattern = pattern::models::Pattern {
+            id: pattern_id.to_owned(),
+            user_id: random_user_id.to_string(),
+            name: Sentence(EN, 5..11).fake(),
+            created_at: DateTime(EN).fake(), 
+        };
+
+        diesel::insert_into(patterns).values(&new_pattern).execute(conn)?;
+        pattern_ids.push(pattern_id);
+        println!("Completed pattern: {}/{}", n, num_of_patterns);
+    }
+
+    Ok(Some(pattern_ids))
+}
+
+/// Populates random number of favorites per user per pattern. Relationship
+/// between user and favorites are unique (e.g., a user can only like a
+/// specific pattern once).
+fn populate_favorites(
+    conn: &SqliteConnection,
+    user_ids: &Vec<String>,
+    pattern_ids: &Vec<String>
+) -> Result<(), DbError> {
+    use crate::favorite;
+    use crate::schema::favorites::dsl::*;
+
+    let mut memoized_favorite = HashSet::new();
+    let num_of_favorites = 350;
+    let mut rng = rand::thread_rng();
+
+    for n in 1..=num_of_favorites {
+        println!("Inserting favorite: {}/{}", n, num_of_favorites);
+        let mut random_user_id = user_ids[rng.gen_range(0..user_ids.len())].to_owned();
+        let mut random_pattern_id = pattern_ids[rng.gen_range(0..pattern_ids.len())].to_owned();
+        let mut memoized_key = random_user_id.to_owned();
+        memoized_key.push_str(&random_pattern_id);
+
+        loop {
+            if memoized_favorite.contains(&memoized_key) {
+                random_user_id = user_ids[rng.gen_range(0..user_ids.len())].to_owned();
+                random_pattern_id = pattern_ids[rng.gen_range(0..pattern_ids.len())].to_owned();
+                memoized_key = random_user_id.to_owned();
+                memoized_key.push_str(&random_pattern_id);
+                continue;
+            }
+
+            break;
+        }
+
+        let new_favorite = favorite::models::Favorite {
+            id: Uuid::new_v4().to_string(),
+            pattern_id: random_pattern_id.to_owned(),
+            user_id: random_user_id.to_owned(),
+        };
+
+        diesel::insert_into(favorites).values(&new_favorite).execute(conn)?;
+
+        memoized_favorite.insert(memoized_key);
+        println!("Completed favorite: {}/{}", n, num_of_favorites);
+    }
+
+    Ok(())
+}
+
+/// Populates database
+pub fn populate_database(
+    conn: &SqliteConnection
+) -> Result<(), DbError> {
+    let user_ids = populate_users(&conn)?.unwrap();
+    let pattern_ids = populate_patterns(&conn, &user_ids)?.unwrap();
+    populate_favorites(&conn, &user_ids, &pattern_ids)
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use actix_web::{App, HttpServer, middleware};
 use diesel::prelude::*;
 use diesel::r2d2::{Pool, ConnectionManager};
 
+pub mod admin;
 pub mod favorite;
 pub mod pattern;
 pub mod schema;
@@ -22,6 +23,7 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to create database connection pool.");
     
     let bind = "127.0.0.1:8080";
+    // let bind = "0.0.0.0:8080";
 
     println!("Starting server at: {}", &bind);
 
@@ -30,6 +32,8 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .data(pool.clone())
             .wrap(middleware::Logger::default())
+            // Admin routes
+            .service(admin::routes::populate)
             // Favorite routes
             .service(favorite::routes::add_favorite)
             // Pattern routes
@@ -51,15 +55,8 @@ fn establish_connection_manager() -> ConnectionManager<SqliteConnection> {
     ConnectionManager::<SqliteConnection>::new(&database_url)
 }
 
-#[cfg(debug_assertions)]
 fn get_database_url() -> std::string::String {
     println!("Getting dev database URL...");
     dotenv::dotenv().ok();
     std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.")
-}
-
-#[cfg(not(debug_assertions))]
-fn get_database_url() -> std::string::String {
-    println!("Getting production database URL...");
-    "production.db".to_string()
 }
